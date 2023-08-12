74ac72f2103206706b0be642facabd2346343fd0
YZ-ST <133514968+YZ-ST@users.noreply.github.com>
2023-08-12 15:32:25 +0000

    完成

--- event_log/test.py
+++ event_log/test.py
@@ -1,5 +1,8 @@
-# from web3 import Web3
-
+from web3 import Web3
+import requests
+from bs4 import BeautifulSoup
+import csv
+# 將十六進制字串轉換為十進制整數
 # def get_event_signature_hash(event_description):
 #     # 创建 web3 实例
 #     w3 = Web3()
@@ -13,11 +16,70 @@
 # print("Event Signature:", event_signature)
 
 
+# 到etherscan指定要爬取的transacion log
+network = "ethereum"
+transaction_hash = '0x50648fa5a012b31e7ec32f974e4f464ac40d4ec6ee9b3291138635907f01b980'
+url = f"https://etherscan.io/tx/{transaction_hash}#eventlog"
 
-original_event_description = "Transfer (index_topic_1 address from, index_topic_2 address to, uint256 value)"
+# 发起HTTP GET请求
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+response = requests.get(url, headers=headers)
+print(response.status_code)
 
-# 移除额外的文本，并替换参数名称
-simplified_event_description = original_event_description.replace("index_topic_1 ", "").replace("index_topic_2 ", "")
+# 检查响应状态码是否为200 (成功)
+if response.status_code == 200:
+    # 使用BeautifulSoup解析HTML内容
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    # 尋找所有以 "funcname_" 开头的<span>元素
+    method = []
+    funcname_spans = soup.find_all("span", id=lambda x: x and x.startswith("funcname_"))
+    # 提取并打印每个<span>元素的内容
+    for span in funcname_spans:
+        span_content = span.get_text(strip=True)
+        modified_text = span_content.replace("View Source", "").strip()
+        # print("Content:",modified_text)
+        method.append(modified_text)
 
-print("Simplified Event Description:", simplified_event_description)
+    
+    # 寻找所有<span class="font-monospace text-break">元素
+    span_elements = soup.find_all("ul", class_="list-unstyled list-xs-space fs-sm mb-0")
+    topic = []
+    for span in span_elements:
+        # 提取span_elements裡所有的<li>元素的<class="font-monospace text-break">
+        li_elements = span.find_all("li")
+        # 使用BeautifulSoup解析HTML内容
+        soup = BeautifulSoup(str(li_elements[0]), "html.parser")
 
+        # 寻找第一个<span class="font-monospace text-break">元素
+        span_element = soup.find("span", class_="font-monospace text-break")
+
+        if span_element:
+            span_content = span_element.get_text(strip=True)
+            # print("Content inside span:", span_content)
+            topic.append(span_content)
+        else:
+            print("No matching span element found.")
+
+    # 將method和topic儲存為key-value pair
+    method_topic = dict(zip(method, topic))
+    print(method_topic)
+
+    # 移除重複的value
+    method_topic = {v: k for k, v in method_topic.items()}
+    print(method_topic)
+
+    # 指定要写入的CSV文件名
+    csv_filename = "method_topic.csv"
+    # 将数据写入CSV文件，可以重複寫入
+    with open(csv_filename, mode="a", newline="", encoding="utf-8") as csv_file:
+        fieldnames = ["network","method", "topic"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        # writer.writeheader()
+        for key, value in method_topic.items():
+            writer.writerow({"network":network,"method": key, "topic": value}) 
+    print(f"Data written to {csv_filename} successfully.")
+else:
+    print("Failed to retrieve the page.")
