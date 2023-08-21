87f3acc4f16ea8dff3926a685ab7b4f4ef5cebbd
YZ-ST <133514968+YZ-ST@users.noreply.github.com>
2023-08-21 07:15:45 +0000

    OK

--- bs4/test.py
+++ bs4/test.py
@@ -3,26 +3,39 @@ from bs4 import BeautifulSoup
 from loguru import logger
 import time
 import re
+import csv
 
-token_address = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-url = f"https://etherscan.io/token/{token_address}"
-
+logger.add("./log/log_{time}.log")
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
-    # Add any other headers you might need
 }
-symbol_response = requests.get(url, headers=headers)
-soup = BeautifulSoup(symbol_response.text, "html.parser")
+first_index = 63200
+# 创建CSV文件并写入标题行
+with open('Tron_contract_address.csv', 'a', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['name', 'symbol', 'contract_address', 'decimal']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
 
-decimal_html = soup.select(
-    f"#ContentPlaceHolder1_divSummary > div.row.g-3.mb-4 > div:nth-child(3) > div > div > div:nth-child(2) > h4")
+    for current_page_index in range(1265, 1559):
+        logger.debug(f"current_page_index: {current_page_index}")
+        url = f"https://apilist.tronscanapi.com/api/tokens/overview?start={first_index}&limit=50&verifier=all&order=desc&filter=trc20&sort=init&order_current=descend"
+        symbol_response = requests.get(url, headers=headers)
+        symbol_info = symbol_response.json()
+        symbol_info = symbol_info["tokens"]
 
-# get decimal_html type to string
-decimal_html_to_string = str(decimal_html[0])
-soup = BeautifulSoup(decimal_html_to_string, 'html.parser')
-# 找到 <b> 標籤
-b_tag = soup.find('b')
-# 獲取 <b> 標籤內容
-decimal = b_tag.get_text()
-logger.debug(f"decimal: {decimal}")
+        for symbol in symbol_info:
+            name = symbol["name"]
+            symbol_abbr = symbol["abbr"]
+            contract_address = symbol["contractAddress"]
+            decimal = symbol["decimal"]
+            logger.debug(f"name: {name}, symbol: {symbol_abbr}, contract_address: {contract_address}, decimal: {decimal}")
 
+            # 将数据写入CSV文件
+            writer.writerow({
+                'name': name,
+                'symbol': symbol_abbr,
+                'contract_address': contract_address,
+                'decimal': decimal
+            })
+
+        first_index += 50
