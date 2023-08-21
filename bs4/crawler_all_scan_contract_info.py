6f6e7dceb7bd7e2dbc49c7a558acb768cc65c5ef
YZ-ST <133514968+YZ-ST@users.noreply.github.com>
2023-08-21 07:20:51 +0000

    all OK

--- bs4/crawler_all_scan_contract_info.py
+++ bs4/crawler_all_scan_contract_info.py
@@ -116,7 +116,39 @@ class CrawlerAllscanContractInfo:
     
     @staticmethod
     def get_tronscan_contract_info():
-        pass
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+        }
+        first_index = 0
+        # 创建CSV文件并写入标题行
+        with open('Tron_contract_address_11.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['name', 'symbol', 'contract_address', 'decimal']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            # 从第一页开始爬取到最後一页
+            for current_page_index in range(1, 1559):
+                logger.debug(f"current_page_index: {current_page_index}")
+                url = f"https://apilist.tronscanapi.com/api/tokens/overview?start={first_index}&limit=50&verifier=all&order=desc&filter=trc20&sort=init&order_current=descend"
+                symbol_response = requests.get(url, headers=headers)
+                symbol_info = symbol_response.json()
+                symbol_info = symbol_info["tokens"]
+
+                for symbol in symbol_info:
+                    name = symbol["name"]
+                    symbol_abbr = symbol["abbr"]
+                    contract_address = symbol["contractAddress"]
+                    decimal = symbol["decimal"]
+                    logger.debug(f"name: {name}, symbol: {symbol_abbr}, contract_address: {contract_address}, decimal: {decimal}")
+
+                    # 将数据写入CSV文件
+                    writer.writerow({
+                        'name': name,
+                        'symbol': symbol_abbr,
+                        'contract_address': contract_address,
+                        'decimal': decimal
+                    })
+
+                first_index += 50
     @staticmethod
     def csv_to_json(csv_file, json_file):
         data = []
@@ -134,6 +166,7 @@ if __name__ == "__main__":
     logger.add("./log/log_{time}.log")
     # CrawlerAllscanContractInfo.get_etherscan_contract_info()
     # CrawlerAllscanContractInfo.get_bscscan_contract_info()
-    csv_file = "Tron_contract_address.csv"
-    json_file = "Tron_contract_address.json"
+    # CrawlerAllscanContractInfo.get_tronscan_contract_info()
+    csv_file = "BSC_contract_address.csv"
+    json_file = "BSC_contract_address.json"
     CrawlerAllscanContractInfo.csv_to_json(csv_file, json_file)
